75db908f540c35860fcd38b1ef56a16a188056ca
Justin Henley <justin.a.henley@gmail.com>
2022-05-06 18:30:13 +0800

    Moved the display of all dishes into own component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
 import './App.css';
-import { useState, useEffect } from 'react';
+//import { useState, useEffect } from 'react';
 //import styled from 'styled-components';
-import DishDisplay from './DishDisplay';
+
+import DishAll from './DishAll';
+import DishSearch from './DishSearch';
 
 function App() {
-  const [data, setData] = useState();
-
-  const getData = async () => {
-    const result = await fetch(
-      'https://menu-translation-backend.herokuapp.com/dishes',
-      {
-        method: 'GET',
-      }
-    );
-
-    const json = await result.json();
-
-    setData(json);
-  };
-
-  useEffect(() => {
-    getData();
-  }, []);
-
   return (
     <div className="App">
       <header className="App-header">Dishes</header>
-      <DishDisplay dishes={data} />
+      <DishAll />
+      <DishSearch />
     </div>
   );
 }
