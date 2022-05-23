c4b54e40fbc56eae2274663a95a778a037893e2b
Justin Henley <justin.a.henley@gmail.com>
2022-05-23 17:35:40 +0800

    dish search now allows for choosing what field to search by

--- src/DishSearch.js
+++ src/DishSearch.js
@@ -7,24 +7,37 @@ import DishDisplay from './DishDisplay';
 // Maybe change to a submit button, OR cache all results locally and search the cache
 
 function DishSearch() {
-  // Search results
-  const [data, setData] = useState();
+  // Search text, type, and results saved in state with initial values
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchType, setSearchType] = useState('en');
+  const [searchResults, setSearchResults] = useState([]);
 
-  // English search term
-  const [searchTerm, setSearchTerm] = useState();
-  const handleChange = async (event) => {
-    // Save the value
+  // Handles changes to the search field
+  const handleInput = async (event) => {
     setSearchTerm(event.target.value);
   };
 
-  // Run the search
-  useEffect(() => {
-    getData(searchTerm);
-  }, [searchTerm]);
+  // Handles selection of the radio buttons
+  const handleClick = (event) => {
+    setSearchType(event.target.value);
+  };
 
-  const getData = async (searchTerm) => {
+  // Run the search whenever either the search type or text changes
+  useEffect(() => {
+    getSearchResults(searchTerm);
+  }, [searchTerm, searchType]);
+
+  const getSearchResults = async (searchTerm) => {
+    // Ensure the search field has a value
+    // TODO regex to check if it is all spaces (should be treated as empty)
+    if (!searchTerm) {
+      setSearchResults([]);
+      return;
+    }
+
+    // Search by search type and text
     const result = await fetch(
-      `https://menu-translation-backend.herokuapp.com/dishes?en=${searchTerm}`,
+      `https://menu-translation-backend.herokuapp.com/dishes?${searchType}=${searchTerm}`,
       {
         method: 'GET',
       }
@@ -32,25 +45,59 @@ function DishSearch() {
 
     const json = await result.json();
 
-    setData(json);
+    setSearchResults(json);
   };
-
-  useEffect(() => {
-    getData();
-  }, []);
 
   return (
     <div className="App">
       <header className="App-header">Search Dishes</header>
-      <div>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <fieldset>
+          <legend>Search by:</legend>
+
+          <div>
+            <input
+              type="radio"
+              id="en-name"
+              name="search-by"
+              value="en"
+              onClick={handleClick}
+              defaultChecked
+            />
+            <label htmlFor="en-name">English name</label>
+          </div>
+
+          <div>
+            <input
+              type="radio"
+              id="zh-name"
+              name="search-by"
+              value="zhtw"
+              onClick={handleClick}
+            />
+            <label htmlFor="zh-name">Chinese Name</label>
+          </div>
+
+          <div>
+            <input
+              type="radio"
+              id="other"
+              name="search-by"
+              value="other"
+              onClick={handleClick}
+            />
+            <label htmlFor="other">Unused</label>
+          </div>
+        </fieldset>
         <input
           type="text"
           placeholder="English Name"
           value={searchTerm}
-          onInput={handleChange}
+          onInput={handleInput}
         />
-      </div>
-      <DishDisplay dishes={data} />
+      </form>
+
+      <DishDisplay dishes={searchResults} />
     </div>
   );
 }
