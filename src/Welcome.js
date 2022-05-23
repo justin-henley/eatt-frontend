58cb41a95b9ca8eb8246ad756376321e9b1243d2
Justin Henley <justin.a.henley@gmail.com>
2022-05-23 15:24:14 +0800

    Created welcome page for new users

---
+++ src/Welcome.js
@@ -0,0 +1,28 @@
+import Dish from './Dish';
+// import styled from 'styled-components';
+
+const Welcome = () => {
+  // A demo dish to label the parts for new users
+  const demoDish = {
+    meat: 'unknown',
+    category: 'unknown',
+    zhtw: 'Traditional Chinese name',
+    pinyin: 'Hanyu Pinyin',
+    en: 'English translation',
+  };
+
+  // The welcome page
+  return (
+    <div>
+      <h1>Welcome to the Menu Translation website!</h1>
+      <p>
+        This website is the interactive front-end for the Menu Translation
+        database, a repository of Taiwanese dishes with their English names
+      </p>
+
+      <Dish item={demoDish} />
+    </div>
+  );
+};
+
+export default Welcome;
