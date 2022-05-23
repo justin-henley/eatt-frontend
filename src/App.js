826fa3f15a9a0e77b0a27fd524753f1375411d8f
Justin Henley <justin.a.henley@gmail.com>
2022-05-23 15:03:51 +0800

    Used react-router in App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 //import { useState, useEffect } from 'react';
 //import styled from 'styled-components';
-
 import DishAll from './DishAll';
 import DishSearch from './DishSearch';
 
 function App() {
-  return (
+  /*return (
     <div className="App">
-      <header className="App-header">Dishes</header>
-      <DishAll />
-      <DishSearch />
+      <Nav />
     </div>
+  );*/
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/welcome">Welcome</Link>
+            </li>
+            <li>
+              <Link to="/dishes">Show All Dishes</Link>
+            </li>
+            <li>
+              <Link to="/search">Search by English Name</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Routes> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+        <Routes>
+          <Route path="/welcome" element={<p>welcome</p>} />
+          <Route path="/dishes" element={<DishAll />} />
+          <Route path="/search" element={<DishSearch />} />
+          <Route path="/" element={<h1>index</h1>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
