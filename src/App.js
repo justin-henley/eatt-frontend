271f7fbdd6e416c76f88df915a9d0abbc9d68042
Justin Henley <justin.a.henley@gmail.com>
2022-05-24 17:05:18 +0800

    Cleaned up App

--- src/App.js
+++ src/App.js
@@ -1,64 +1,48 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 //import { useState, useEffect } from 'react';
-import styled from 'styled-components';
+//import styled from 'styled-components';
 import DishAll from './DishAll';
 import DishSearch from './DishSearch';
 import Welcome from './Welcome';
 
 function App() {
-  /*return (
-    <div className="App">
-      <Nav />
-    </div>
-  );*/
-  const n = (
-    <nav class="navbar">
-      <div class="logo">
-        <Link to="/">Menu Translation</Link>
-      </div>
-
-      <ul class="nav-links">
-        <input type="checkbox" id="checkbox_toggle" />
-        <label for="checkbox_toggle" class="hamburger">
-          &#9776;
-        </label>
-
-        <div class="menu">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/dishes">Show All Dishes</Link>
-          </li>
-          <li>
-            <Link to="/search">Search Dishes</Link>
-          </li>
-        </div>
-      </ul>
-    </nav>
-  );
-
   return (
     <Router>
-      <StyledApp>
-        {n}
+      <div>
+        <nav class="navbar">
+          <div class="logo">
+            <Link to="/">Menu Translation</Link>
+          </div>
 
-        {/* A <Routes> looks through its children <Route>s and
-          renders the first one that matches the current URL. */}
+          <ul class="nav-links">
+            <input type="checkbox" id="checkbox_toggle" />
+            <label for="checkbox_toggle" class="hamburger">
+              &#9776;
+            </label>
+
+            <div class="menu">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/dishes">Show All Dishes</Link>
+              </li>
+              <li>
+                <Link to="/search">Search Dishes</Link>
+              </li>
+            </div>
+          </ul>
+        </nav>
+
         <Routes>
           <Route path="/dishes" element={<DishAll />} />
           <Route path="/search" element={<DishSearch />} />
           <Route path="/" element={<Welcome />} />
         </Routes>
-      </StyledApp>
+      </div>
     </Router>
   );
 }
 
 export default App;
-
-const StyledApp = styled.div`
-  width: 90vw;
-  margin: 2em auto;
-`;
