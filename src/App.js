4a09093c0431ca6fccc73ca6db04a76c5630023a
Justin Henley <justin.a.henley@gmail.com>
2022-05-24 20:03:09 +0800

    Stored page data in object; sigle source of truth

--- src/App.js
+++ src/App.js
@@ -6,39 +6,75 @@ import DishAll from './DishAll';
 import DishSearch from './DishSearch';
 import Welcome from './Welcome';
 
+class PageRecord extends Object {
+  constructor(name, link, element) {
+    super();
+    this.name = name;
+    this.link = link;
+    this.element = element;
+  }
+}
+
+const PageRecords = {
+  home: new PageRecord('Home', '/', <Welcome />),
+  dishes: new PageRecord('Show All Dishes', '/dishes', <DishAll />),
+  search: new PageRecord('Search Dishes', '/search', <DishSearch />),
+  adv: new PageRecord('Advanced Search', '/adv-search', <Welcome />),
+};
+
 function App() {
   return (
     <Router>
       <div>
         <nav className={styles.navbar}>
           <div className={styles.logo}>
-            <Link to="/">Menu Translation</Link>
+            <Link to={PageRecords.home.link}>Menu Translation</Link>
           </div>
 
           <ul className={styles.navLinks}>
             <input type="checkbox" id="checkbox_toggle" />
-            <label for="checkbox_toggle" className={styles.hamburger}>
+            <label htmlFor="checkbox_toggle" className={styles.hamburger}>
               &#9776;
             </label>
 
             <div className={styles.menu}>
               <li>
-                <Link to="/">Home</Link>
+                <Link to={PageRecords.home.link}>{PageRecords.home.name}</Link>
               </li>
               <li>
-                <Link to="/dishes">Show All Dishes</Link>
+                <Link to={PageRecords.dishes.link}>
+                  {PageRecords.dishes.name}
+                </Link>
               </li>
               <li>
-                <Link to="/search">Search Dishes</Link>
+                <Link to={PageRecords.search.link}>
+                  {PageRecords.search.name}
+                </Link>
+              </li>
+              <li>
+                <Link to={PageRecords.adv.link}>{PageRecords.adv.name}</Link>
               </li>
             </div>
           </ul>
         </nav>
         <div className={styles.bodyArea}>
           <Routes>
-            <Route path="/dishes" element={<DishAll />} />
-            <Route path="/search" element={<DishSearch />} />
-            <Route path="/" element={<Welcome />} />
+            <Route
+              path={PageRecords.adv.link}
+              element={PageRecords.adv.element}
+            />
+            <Route
+              path={PageRecords.dishes.link}
+              element={PageRecords.dishes.element}
+            />
+            <Route
+              path={PageRecords.search.link}
+              element={PageRecords.search.element}
+            />
+            <Route
+              path={PageRecords.home.link}
+              element={PageRecords.home.element}
+            />
           </Routes>
         </div>
       </div>
