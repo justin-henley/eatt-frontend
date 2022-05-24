941b48408af3534a45b5d3707f3f0031f9e4cd5f
Justin Henley <justin.a.henley@gmail.com>
2022-05-24 15:22:33 +0800

    Found a more terse way to style icons

--- src/Dish.js
+++ src/Dish.js
@@ -12,61 +12,53 @@ import {
 import { FaFish, FaBreadSlice, FaQuestionCircle } from 'react-icons/fa';
 
 const Dish = ({ item }) => {
-  let meatIcon, categoryIcon;
+  // Lists of meat and category icons
+  const icons = {
+    meat: {
+      beef: <SiHappycow />,
+      pork: <GiPig />,
+      bird: <GiChicken />,
+      fish: <FaFish />,
+      veg: <GiBroccoli />,
+      other: <FaQuestionCircle />,
+      unknown: <FaQuestionCircle />,
+    },
+    category: {
+      rice: <GiBowlOfRice />,
+      noodle: <GiNoodles />,
+      bread: <FaBreadSlice />,
+      soup: <GiBubblingBowl />,
+      drink: <GiCoffeeCup />,
+      other: <FaQuestionCircle />,
+      unknown: <FaQuestionCircle />,
+    },
+  };
 
-  // Choose the meat icon
-  meatIcon =
-    item.meat === 'beef' ? (
-      <BeefIcon />
-    ) : item.meat === 'pork' ? (
-      <PorkIcon />
-    ) : item.meat === 'bird' ? (
-      <BirdIcon />
-    ) : item.meat === 'fish' ? (
-      <FishIcon />
-    ) : item.meat === 'veg' ? (
-      <VegIcon />
-    ) : item.meat === 'other' ? (
-      <OtherIcon />
-    ) : item.meat === 'unknown' ? (
-      <UnknownIcon />
-    ) : null;
-
-  // Choose the category icon
-  categoryIcon =
-    item.category === 'rice' ? (
-      <RiceIcon />
-    ) : item.category === 'noodle' ? (
-      <NoodleIcon />
-    ) : item.category === 'bread' ? (
-      <BreadIcon />
-    ) : item.category === 'soup' ? (
-      <SoupIcon />
-    ) : item.category === 'drink' ? (
-      <DrinkIcon />
-    ) : item.category === 'other' ? (
-      <OtherIcon />
-    ) : item.category === 'unknown' ? (
-      <UnknownIcon />
-    ) : null;
+  // Select the meat and category icons for this dish
+  const meatIcon = icons.meat[item.meat];
+  const categoryIcon = icons.category[item.category];
 
   return (
     <Tile>
-      <h1>{item.zhtw}</h1>
-      <h3>{item.pinyin}</h3>
-      <p>{item.en}</p>
-      <div>
+      <Icons>
         {meatIcon}
         {categoryIcon}
-      </div>
+      </Icons>
+      <Text>
+        <Chinese>{item.zhtw}</Chinese>
+        <Translation>
+          <Pinyin>{item.pinyin}</Pinyin>
+          <English>{item.en}</English>
+        </Translation>
+      </Text>
     </Tile>
   );
 };
 
 export default Dish;
 
-// Dish tile style
-const Tile = styled.div`
+// Dish tile style BOXY
+/* const Tile = styled.div`
   width: 50%;
   max-width: 200px;
   padding: 1em;
@@ -74,48 +66,69 @@ const Tile = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+`; */
+// WIDE
+const Tile = styled.div`
+  width: 100%;
+  height: 4em;
+  //max-width: 400px;
+  //padding: 1em;
+  margin: 0.1em;
+  border: 1px solid gray;
+  display: flex;
+  flex-direction: row;
+  //align-items: center;
+  //justify-content: space-between
 `;
 
-// Styling for Meat and Category Icons
-const iconStyle = `
-margin: 5px;
-font-size: 2em;
-color: gray;
+const Text = styled.div`
+  //background-color: gray;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
 `;
-// Icons
-const BeefIcon = styled(SiHappycow)`
-  ${iconStyle}
+
+const Chinese = styled.h1`
+  //color: red;
+  font-size: 2.5em;
+  text-align: end;
+  width: 50%;
+  padding-right: 0.1em;
+  //border: 1px solid blue;
+  &:hover {
+    color: red;
+  }
 `;
-const PorkIcon = styled(GiPig)`
-  ${iconStyle}
+
+const Translation = styled.div`
+  width: 50%;
+  //border: 1px solid red;
+  padding-left: 0.1em;
 `;
-const BirdIcon = styled(GiChicken)`
-  ${iconStyle}
+
+const Pinyin = styled.h2`
+  //color: blue;
+  font-size: 1.5em;
 `;
-const FishIcon = styled(FaFish)`
-  ${iconStyle}
+
+const English = styled.h3`
+  //color: green;
+  font-size: 0.75em;
 `;
-const VegIcon = styled(GiBroccoli)`
-  ${iconStyle}
-`;
-const RiceIcon = styled(GiBowlOfRice)`
-  ${iconStyle}
-`;
-const SoupIcon = styled(GiBubblingBowl)`
-  ${iconStyle}
-`;
-const NoodleIcon = styled(GiNoodles)`
-  ${iconStyle}
-`;
-const BreadIcon = styled(FaBreadSlice)`
-  ${iconStyle}
-`;
-const UnknownIcon = styled(FaQuestionCircle)`
-  ${iconStyle}
-`;
-const OtherIcon = styled(FaQuestionCircle)`
-  ${iconStyle}
-`;
-const DrinkIcon = styled(GiCoffeeCup)`
-  ${iconStyle}
+
+const Icons = styled.div`
+  background-color: darkgrey;
+  height: 100%;
+  width: 3em;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  // Selects the children, which should only be icons
+  & > * {
+    color: gray;
+    font-size: 2em;
+  }
 `;
