7e774980bf681232591d5aabd358e81be132980c
Justin Henley <justin.a.henley@gmail.com>
2022-05-24 17:39:06 +0800

    Removed styled components; moved css into modules

--- src/Dish.js
+++ src/Dish.js
@@ -1,4 +1,6 @@
-import styled from 'styled-components';
+import styles from './Dish.module.css'; // Import css modules stylesheet as styles
+
+//import styled from 'styled-components';
 import { SiHappycow } from 'react-icons/si';
 import {
   GiPig,
@@ -12,123 +14,47 @@ import {
 import { FaFish, FaBreadSlice, FaQuestionCircle } from 'react-icons/fa';
 
 const Dish = ({ item }) => {
-  // Lists of meat and category icons
-  const icons = {
-    meat: {
-      beef: <SiHappycow />,
-      pork: <GiPig />,
-      bird: <GiChicken />,
-      fish: <FaFish />,
-      veg: <GiBroccoli />,
-      other: <FaQuestionCircle />,
-      unknown: <FaQuestionCircle />,
-    },
-    category: {
-      rice: <GiBowlOfRice />,
-      noodle: <GiNoodles />,
-      bread: <FaBreadSlice />,
-      soup: <GiBubblingBowl />,
-      drink: <GiCoffeeCup />,
-      other: <FaQuestionCircle />,
-      unknown: <FaQuestionCircle />,
-    },
-  };
-
   // Select the meat and category icons for this dish
   const meatIcon = icons.meat[item.meat];
   const categoryIcon = icons.category[item.category];
 
   return (
-    <Tile>
-      <Icons>
+    <div className={styles.tile}>
+      <div className={styles.icons}>
         {meatIcon}
         {categoryIcon}
-      </Icons>
-      <Text>
-        <Chinese>{item.zhtw}</Chinese>
-        <Translation>
-          <Pinyin>{item.pinyin}</Pinyin>
-          <English>{item.en}</English>
-        </Translation>
-      </Text>
-    </Tile>
+      </div>
+      <div className={styles.text}>
+        <h1 className={styles.chinese}>{item.zhtw}</h1>
+        <div className={styles.translation}>
+          <h2 className={styles.pinyin}>{item.pinyin}</h2>
+          <h3 className={styles.english}>{item.en}</h3>
+        </div>
+      </div>
+    </div>
   );
 };
 
+// Lists of meat and category icons
+const icons = {
+  meat: {
+    beef: <SiHappycow />,
+    pork: <GiPig />,
+    bird: <GiChicken />,
+    fish: <FaFish />,
+    veg: <GiBroccoli />,
+    other: <FaQuestionCircle />,
+    unknown: <FaQuestionCircle />,
+  },
+  category: {
+    rice: <GiBowlOfRice />,
+    noodle: <GiNoodles />,
+    bread: <FaBreadSlice />,
+    soup: <GiBubblingBowl />,
+    drink: <GiCoffeeCup />,
+    other: <FaQuestionCircle />,
+    unknown: <FaQuestionCircle />,
+  },
+};
+
 export default Dish;
-
-// Dish tile style BOXY
-/* const Tile = styled.div`
-  width: 50%;
-  max-width: 200px;
-  padding: 1em;
-  border: 1px solid gray;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`; */
-// WIDE
-const Tile = styled.div`
-  width: 100%;
-  height: 4em;
-  //max-width: 400px;
-  //padding: 1em;
-  margin: 0.1em;
-  border: 1px solid gray;
-  display: flex;
-  flex-direction: row;
-  //align-items: center;
-  //justify-content: space-between
-`;
-
-const Text = styled.div`
-  //background-color: gray;
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-`;
-
-const Chinese = styled.h1`
-  //color: red;
-  font-size: 2.5em;
-  text-align: end;
-  width: 50%;
-  padding-right: 0.1em;
-  //border: 1px solid blue;
-  &:hover {
-    color: red;
-  }
-`;
-
-const Translation = styled.div`
-  width: 50%;
-  //border: 1px solid red;
-  padding-left: 0.1em;
-`;
-
-const Pinyin = styled.h2`
-  //color: blue;
-  font-size: 1.5em;
-`;
-
-const English = styled.h3`
-  //color: green;
-  font-size: 0.75em;
-`;
-
-const Icons = styled.div`
-  background-color: darkgrey;
-  height: 100%;
-  width: 3em;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  // Selects the children, which should only be icons
-  & > * {
-    color: gray;
-    font-size: 2em;
-  }
-`;
