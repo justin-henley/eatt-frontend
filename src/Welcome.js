5394ea20aee4b66639122e4fa38226c9d03b2bfe
Justin Henley <justin.a.henley@gmail.com>
2022-05-27 01:54:04 +0800

    Added images to homepage and styled

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,26 +1,57 @@
 import Dish from './Dish';
 // import styled from 'styled-components';
+import styles from './Welcome.module.css';
 
 const Welcome = () => {
   // A demo dish to label the parts for new users
-  const demoDish = {
-    meat: 'unknown',
-    category: 'unknown',
-    zhtw: 'Chinese',
-    pinyin: 'Hanyu Pinyin',
-    en: 'English translation',
+  const eatTile = {
+    meat: 'pork',
+    zhtw: '刈包',
+    pinyin: 'guàbāo',
+    en: 'Pork Belly Bun',
+  };
+
+  const drinkTile = {
+    category: 'drink',
+    zhtw: '珍珠奶茶',
+    pinyin: 'zhēnzhū nǎichá',
+    en: 'Bubble Milk Tea',
   };
 
   // The welcome page
   return (
     <div>
-      <h1>Welcome to the Menu Translation website!</h1>
+      <div className={styles.block1}>
+        <img
+          alt="Gua Bao"
+          src="./img/pexels-adrian-dorobantu-2089712.jpg"
+          height="auto"
+        />
+        <div>
+          <p className={styles.call}>EAT</p>
+          <p className={styles.response}>your heart out</p>
+          <Dish item={eatTile} />
+        </div>
+      </div>
+      <div class={styles.block2}>
+        <img
+          src="./img/taiwan-pearl-milk-tea-with-bubble-ximending-taipei-taiwan.jpg"
+          alt="Milk Tea"
+          height="auto"
+        />
+        <div>
+          <p className={styles.call}>DRINK</p>
+          <p className={styles.response}>and be merry.</p>
+          <Dish item={drinkTile} />
+        </div>
+      </div>
+      {/* <h1>Welcome to the Menu Translation website!</h1>
       <p>
         This website is the interactive front-end for the Menu Translation
         database, a repository of Taiwanese dishes with their English names.
       </p>
 
-      <Dish item={demoDish} />
+      <Dish item={demoDish} /> */}
     </div>
   );
 };
