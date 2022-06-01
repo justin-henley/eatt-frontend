7c7ee8b3d3fd7d0cbfcf74a8bbf8f5c7736f9003
Justin Henley <justin.a.henley@gmail.com>
2022-06-01 22:17:06 +0800

    AllMenus complete

--- src/Menus/AllMenus.js
+++ src/Menus/AllMenus.js
@@ -1,5 +1,32 @@
+import { useState, useEffect } from 'react';
+import MenuDisplay from './MenuDisplay';
+
 function AllMenus() {
-  return <h1>AllMenus Component</h1>;
+  const [data, setData] = useState();
+
+  const getData = async () => {
+    const result = await fetch(
+      'https://menu-translation-backend.herokuapp.com/menus',
+      {
+        method: 'GET',
+      }
+    );
+
+    const json = await result.json();
+
+    setData(json);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div>
+      <h1>AllMenus Component</h1>
+      <MenuDisplay menus={data} />
+    </div>
+  );
 }
 
 export default AllMenus;
