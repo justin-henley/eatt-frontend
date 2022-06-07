7cddc96ea65ff86e87574fdd18f575e60926f790
Justin Henley <justin.a.henley@gmail.com>
2022-06-07 16:41:18 +0800

    Removed component label headers

--- src/Menus/MenuDisplay.js
+++ src/Menus/MenuDisplay.js
@@ -4,7 +4,6 @@ import MenuPreviewTile from './MenuPreviewTile';
 const MenuDisplay = ({ menus }) => {
   return (
     <div>
-      <h1>MenuDisplay Component</h1>
       {menus?.map((menu) => (
         <>
           {' '}
