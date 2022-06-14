8483168aafbeabea5ae451338d0b4272ae3849d3
Justin Henley <justin.a.henley@gmail.com>
2022-06-14 17:41:27 +0800

    Removed empty components

--- src/Menus/MenuDisplay.js
+++ src/Menus/MenuDisplay.js
@@ -1,18 +1,15 @@
 // Outside components
 import { Link } from 'react-router-dom';
 // Custom components
-import MenuPreviewTile from './MenuPreviewTile';
+import MenuName from './MenuName';
 
 const MenuDisplay = ({ menus }) => {
   return (
     <div>
       {menus?.map((menu) => (
-        <>
-          {' '}
-          <Link to={`/menus/${menu._id}`}>
-            <MenuPreviewTile key={menu._id} item={menu} />
-          </Link>
-        </>
+        <Link to={`/menus/${menu._id}`}>
+          <MenuName key={menu._id} item={menu} />
+        </Link>
       ))}
     </div>
   );
