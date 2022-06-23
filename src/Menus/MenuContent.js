9c89a3f4344d57db81d69e4eef5250a347ead2b7
Justin Henley <justin.a.henley@gmail.com>
2022-06-23 21:22:48 +0800

    Factored out duplicated code from MenuCategory

--- src/Menus/MenuContent.js
+++ src/Menus/MenuContent.js
@@ -2,6 +2,7 @@
 
 // Custom Components
 import DishTile from '../Dishes/DishTile';
+import MenuCategory from './MenuCategory';
 
 // CSS
 import styles from './MenuContent.module.css';
@@ -11,18 +12,7 @@ function MenuContent({ menu }) {
     <div>
       <h1 className={styles.menu}>Menu</h1>
       {menu?.map((category) => (
-        <div key={category._id} className={styles.category}>
-          <div className={styles.categoryName}>
-            <h3 className={styles.categoryName__zhtw}>{category.zhtw}</h3>
-            <h3 className={styles.categoryName__pinyin}>{category.pinyin}</h3>
-            <h3 className={styles.categoryName__en}>{category.en}</h3>
-          </div>
-          <div className={styles.dishArea}>
-            {category.items?.map((item) => (
-              <DishTile key={item._id} item={item} />
-            ))}
-          </div>
-        </div>
+        <MenuCategory category={category} key={category._id} />
       ))}
     </div>
   );
