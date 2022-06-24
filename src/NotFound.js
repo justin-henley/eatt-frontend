721532db6e89ed75b146438c89355453be30c25a
Justin Henley <justin.a.henley@gmail.com>
2022-06-25 03:28:56 +0800

    Added error message prop to 404 page

--- src/NotFound.js
+++ src/NotFound.js
@@ -1,5 +1,18 @@
-function NotFound() {
-  return <p>Page Not Found</p>;
+// Icon
+import { MdNoFood } from 'react-icons/md';
+// CSS
+import styles from './NotFound.module.css';
+
+function NotFound({ message }) {
+  return (
+    <div className={styles.container}>
+      <MdNoFood size="250" className={styles.foodIcon} />
+      <div className={styles.text}>
+        <h1 className={styles.notFound}>Page Not Found</h1>
+        <h2 className={styles.message}>{message || '404'}</h2>
+      </div>
+    </div>
+  );
 }
 
 export default NotFound;
