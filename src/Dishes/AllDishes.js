be0dac9175098937de5e89a6b0036dc163372056
Justin Henley <justin.a.henley@gmail.com>
2022-06-28 14:47:36 +0800

    Added styling to AllDishes

--- src/Dishes/AllDishes.js
+++ src/Dishes/AllDishes.js
@@ -2,18 +2,17 @@
 import { useState, useEffect } from 'react';
 // Custom components
 import DishDisplay from './DishDisplay';
+// CSS
+import styles from './AllDishes.module.css';
 
 function AllDishes() {
   // Dish data is set in state and updated upon fetching
   const [data, setData] = useState();
 
   const getData = async () => {
-    const result = await fetch(
-      'https://menu-translation-backend.herokuapp.com/dishes',
-      {
-        method: 'GET',
-      }
-    );
+    const result = await fetch('https://menu-translation-backend.herokuapp.com/dishes', {
+      method: 'GET',
+    });
 
     const json = await result.json();
 
@@ -26,7 +25,8 @@ function AllDishes() {
 
   return (
     <div>
-      <h1 className="App-header">Dishes</h1>
+      <h1 className={styles.title}>Dishes</h1>
+      <hr className={styles.rule} />
       <DishDisplay dishes={data} />
     </div>
   );
