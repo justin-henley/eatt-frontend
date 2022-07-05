559bd309ff4e4aeff4cdedb524081b92e4f531f1
Justin Henley <justin.a.henley@gmail.com>
2022-07-05 19:14:30 +0800

    Moved homepage and styles

--- pages/_app.js
+++ pages/_app.js
@@ -4,14 +4,13 @@
 import GlobalNav from '../components/GlobalNav';
 import GlobalFooter from '../components/GlobalFooter';
 // CSS
-import styles from '../styles/App.module.css';
 import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <div>
       <GlobalNav />
-      <div className={styles.bodyDiv}>
+      <div className="bodyDiv">
         <Component {...pageProps} />
       </div>
       <GlobalFooter />
