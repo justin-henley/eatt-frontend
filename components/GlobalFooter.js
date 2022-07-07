5da1fb3eea3275f2811bd8a72c4983bfc0777797
Justin Henley <justin.a.henley@gmail.com>
2022-07-07 21:29:36 +0800

    Restored attribution link for bubble tea image

--- components/GlobalFooter.js
+++ components/GlobalFooter.js
@@ -5,10 +5,8 @@ function GlobalFooter() {
   return (
     <footer className={styles.foot}>
       Favicon courtesy of <a href="https://icons8.com/">icons8</a>
-      {/* <a href="https://www.freepik.com/photos/pearl-tea">
-            <br />
-            Pearl tea photo created by tawatchai07 - www.freepik.com
-          </a> */}
+      <br />
+      Pearl tea photo created by tawatchai07 - <a href="https://www.freepik.com/photos/pearl-tea">www.freepik.com</a>
       <br />
       Eatt &copy;2022 Justin Henley
     </footer>
