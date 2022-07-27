8f3dea3443e2a184e83f8ca25c1f189ac0a513cb
Justin Henley <justin.a.henley@gmail.com>
2022-07-27 23:10:52 +0800

    bug hunting

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 import AuthContext from '../context/AuthProvider';
 
 const useAuth = () => {
+  console.log(AuthContext);
   return useContext(AuthContext);
 };
 
