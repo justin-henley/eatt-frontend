0e603d48593d3655967a5f86557c8f7efa10be73
Justin Henley <justin.a.henley@gmail.com>
2022-07-27 23:29:15 +0800

    Moved login into pages directory

--- pages/login.js
+++ pages/login.js
@@ -2,7 +2,7 @@
 import { useRef, useState, useEffect } from 'react';
 import useAuth from '../hooks/useAuth';
 // Axios
-import axios from '../pages/api/axios';
+import axios from './api/axios';
 const LOGIN_URL = '/auth';
 
 const Login = () => {
