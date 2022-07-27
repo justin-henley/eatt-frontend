9d7c052efa99affd6082f858651769e09c1be14a
Justin Henley <justin.a.henley@gmail.com>
2022-07-27 23:19:44 +0800

    Added AuthProvider; solved login troubles

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 // Libraries
 import Head from 'next/head';
 import SSRProvider from 'react-bootstrap/SSRProvider';
+// Context
+import { AuthProvider } from '../context/AuthProvider';
 // Hooks
 import useAuth from '../hooks/useAuth';
 // Custom components
@@ -13,33 +15,35 @@ import '../styles/globals.css';
 export default function MyApp({ Component, pageProps }) {
   return (
     <SSRProvider>
-      <div>
-        <Head>
-          <title>Eatt</title>
-          <meta
-            name="description"
-            content="Expand your meals with bilingual menus for your favorite local restaurants"
-          />
-          <meta name="keywords" content="translation, menu, chinese, english, mandarin, taiwan" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <link rel="icon" href="/favicon.ico" />
-
-          <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
-        </Head>
+      <AuthProvider>
         <div>
-          <GlobalNav />
-          {/* Check if the page requires authorization. Only contacts /api/auth/session endpoint for pages requiring auth */}
-          {Component.auth ? (
-            <RequireAuth>
+          <Head>
+            <title>Eatt</title>
+            <meta
+              name="description"
+              content="Expand your meals with bilingual menus for your favorite local restaurants"
+            />
+            <meta name="keywords" content="translation, menu, chinese, english, mandarin, taiwan" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <link rel="icon" href="/favicon.ico" />
+
+            <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+          </Head>
+          <div>
+            <GlobalNav />
+            {/* Check if the page requires authorization. Only contacts /api/auth/session endpoint for pages requiring auth */}
+            {Component.auth ? (
+              <RequireAuth>
+                <Component {...pageProps} />
+              </RequireAuth>
+            ) : (
+              /* Render without auth */
               <Component {...pageProps} />
-            </RequireAuth>
-          ) : (
-            /* Render without auth */
-            <Component {...pageProps} />
-          )}
-          <GlobalFooter />
+            )}
+            <GlobalFooter />
+          </div>
         </div>
-      </div>
+      </AuthProvider>
     </SSRProvider>
   );
 }
