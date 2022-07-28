e2e95ec4413aa811a5d193ca117b59d2c5e6b245
Justin Henley <justin.a.henley@gmail.com>
2022-07-29 00:08:45 +0800

    Nav links changed to NextJS Link;

--- components/GlobalNav.js
+++ components/GlobalNav.js
@@ -1,5 +1,6 @@
 // Libraries
 import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import Link from 'next/link';
 // CSS
 import styles from '../styles/GlobalNav.module.css';
 
@@ -8,29 +9,46 @@ function GlobalNav() {
   return (
     <Navbar expand="lg" sticky="top" collapseOnSelect className={styles.navbar}>
       <Container>
-        <Navbar.Brand href="/" className={styles.navLogo}>
-          eatt
+        <Navbar.Brand className={styles.navLogo}>
+          <Link href="/" className={styles.navLogo}>
+            eatt
+          </Link>
         </Navbar.Brand>
 
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav>
-            <Nav.Link href="/" /* className={styles.navText} */>Home</Nav.Link>
+            <Nav.Link>
+              {/* TODO restyle */}
+              <Link href="/">Home</Link>
+            </Nav.Link>
 
             {/* Contains the links to all dish-related pages */}
             <NavDropdown title="Dishes">
-              <NavDropdown.Item href="/dishes">All Dishes</NavDropdown.Item>
-              <NavDropdown.Item href="/dishes/search">Search Dishes</NavDropdown.Item>
+              <NavDropdown.Item>
+                <Link href="/dishes">All Dishes</Link>
+              </NavDropdown.Item>
+              <NavDropdown.Item>
+                <Link href="/dishes/search">Search Dishes</Link>
+              </NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="/dishes/new">Create New Dish</NavDropdown.Item>
+              <NavDropdown.Item>
+                <Link href="/dishes/new">Create New Dish</Link>
+              </NavDropdown.Item>
             </NavDropdown>
 
             {/* Contains the links to all menu-related pages */}
             <NavDropdown title="Menus" id="nav-dropdown">
-              <NavDropdown.Item href="/menus">All Menus</NavDropdown.Item>
-              <NavDropdown.Item href="/menus/search">Search Menus</NavDropdown.Item>
+              <NavDropdown.Item>
+                <Link href="/menus">All Menus</Link>
+              </NavDropdown.Item>
+              <NavDropdown.Item>
+                <Link href="/menus/search">Search Menus</Link>
+              </NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="/menus/new">Create New Menu</NavDropdown.Item>
+              <NavDropdown.Item>
+                <Link href="/menus/new">Create New Menu</Link>
+              </NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
