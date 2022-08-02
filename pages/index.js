a19549920b50adb0a1c1abc56c9b798fad3cf9fc
Justin Henley <justin.a.henley@gmail.com>
2022-08-02 22:17:16 +0800

    Added image carousel and new image sources

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 // Libraries
 import Image from 'next/image';
+import { Carousel } from 'react-bootstrap';
 // Custom Components
 import DishTile from '../components/Dish/DishTile';
 // CSS
 import styles from '../styles/Home.module.css';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 // Images
-import bannerGuabao from '../public/img/pexels-adrian-dorobantu-2089712.jpg';
-/* import bannerMilkTea from '../public/img/taiwan-pearl-milk-tea-with-bubble-ximending-taipei-taiwan.jpg'; */
+import bannerGuabao from '../public/img/guabao.jpg';
+import bannerMilkTea from '../public/img/tea.jpg';
 
 export default function Home() {
   // A demo dish to label the parts for new users
@@ -28,39 +30,52 @@ export default function Home() {
   // Keep an eye out for further image/delivery optimization for further speed gains
   return (
     <div>
-      <div className={styles.block}>
-        <h1 className={styles.slogan}>
-          <span className={styles.sloganFlash}>Eat</span> your heart out
-        </h1>
-        <div className={styles.imageWrapper}>
-          <Image
-            alt="Gua Bao"
-            src={bannerGuabao}
-            placeholder="blur"
-            width={625}
-            height={500}
-            layout="responsive"
-            className={styles.image}
-            priority="true"
-          />
-        </div>
-        <div className={styles.dish}>
-          <DishTile item={eatTile} className={styles.food} />{' '}
-        </div>
-      </div>
-      {/* {
-        <div className={styles.block}>
-          <h1 className={styles.slogan}>
-            <span className={styles.sloganFlash}>Drink</span> and be merry.
-          </h1>
-          <div className={styles.imageWrapper}>
-            <Image alt="Bubble Milk Tea" src={bannerMilkTea} placeholder="blur" layout="responsive" priority className={styles.image} />
+      <Carousel>
+        <Carousel.Item>
+          <div className={styles.block}>
+            <h1 className={styles.slogan}>
+              <span className={styles.sloganFlash}>Eat</span> your heart out
+            </h1>
+            <div className={styles.imageWrapper}>
+              <Image
+                alt="Gua Bao"
+                src={bannerGuabao}
+                placeholder="blur"
+                width={800}
+                height={529}
+                layout="responsive"
+                className={styles.image}
+                priority="true"
+              />
+            </div>
+            <div className={styles.dish}>
+              <DishTile item={eatTile} className={styles.food} />{' '}
+            </div>
           </div>
-          <div className={styles.dish}>
-            <DishTile item={drinkTile} />{' '}
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className={styles.block}>
+            <h1 className={styles.slogan}>
+              <span className={styles.sloganFlash}>Drink</span> and be merry.
+            </h1>
+            <div className={styles.imageWrapper}>
+              <Image
+                alt="Bubble Milk Tea"
+                src={bannerMilkTea}
+                placeholder="blur"
+                layout="responsive"
+                width={800}
+                height={529}
+                priority
+                className={styles.image}
+              />
+            </div>
+            <div className={styles.dish}>
+              <DishTile item={drinkTile} />{' '}
+            </div>
           </div>
-        </div>
-      } */}
+        </Carousel.Item>
+      </Carousel>
     </div>
   );
 }
