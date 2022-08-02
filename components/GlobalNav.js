ee52bfbeb747b67f846342e7fc9e4e0519e1ed2f
Justin Henley <justin.a.henley@gmail.com>
2022-08-02 18:26:17 +0800

    Added login/logout button to nav

--- components/GlobalNav.js
+++ components/GlobalNav.js
@@ -1,6 +1,9 @@
 // Libraries
 import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+// Hooks
+import useAuth from '../hooks/useAuth';
 // CSS
 import styles from '../styles/GlobalNav.module.css';
 
@@ -17,11 +20,11 @@ function GlobalNav() {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav>
             <Link href="/" passHref>
-              <Nav.Link>Home</Nav.Link>
+              <Nav.Link className={styles.navText}>Home</Nav.Link>
             </Link>
 
             {/* Contains the links to all dish-related pages */}
-            <NavDropdown title="Dishes">
+            <NavDropdown title={<span className={styles.navText}>Dishes</span>}>
               <Link href="/dishes" passHref>
                 <NavDropdown.Item>All Dishes</NavDropdown.Item>
               </Link>
@@ -37,7 +40,7 @@ function GlobalNav() {
             </NavDropdown>
 
             {/* Contains the links to all menu-related pages */}
-            <NavDropdown title="Menus" id="nav-dropdown">
+            <NavDropdown title={<span className={styles.navText}>Menus</span>}>
               <Link href="/menus" passHref>
                 <NavDropdown.Item>All Menus</NavDropdown.Item>
               </Link>
@@ -51,6 +54,7 @@ function GlobalNav() {
                 <NavDropdown.Item>Create New Menu</NavDropdown.Item>
               </Link>
             </NavDropdown>
+            <LogInOut />
           </Nav>
         </Navbar.Collapse>
       </Container>
@@ -59,3 +63,24 @@ function GlobalNav() {
 }
 
 export default GlobalNav;
+
+const LogInOut = () => {
+  const { auth, setAuth } = useAuth();
+  const router = useRouter();
+
+  return auth?.user ? (
+    <button
+      className={styles.logBtn}
+      onClick={() => {
+        setAuth({});
+        router.push('/');
+      }}
+    >
+      Log Out
+    </button>
+  ) : (
+    <button className={styles.logBtn} onClick={() => router.push('/login')}>
+      Log In
+    </button>
+  );
+};
