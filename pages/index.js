315816320453d33d6efb30cdf4fdf374fd8f9a3e
Justin Henley <justin.a.henley@gmail.com>
2022-08-03 23:12:17 +0800

    Styled homepage

--- pages/index.js
+++ pages/index.js
@@ -29,72 +29,81 @@ export default function Home() {
   // Keep an eye out for further image/delivery optimization for further speed gains
   return (
     <div className={styles.wrapper}>
-      <Carousel>
-        <Carousel.Item>
-          <div className={styles.block}>
-            <h1 className={styles.slogan}>
-              <span className={styles.sloganFlash}>Eat</span> your heart out
-            </h1>
-            <div className={styles.imageWrapper}>
-              <Image
-                alt="Gua Bao"
-                src={bannerGuabao}
-                placeholder="blur"
-                width={800}
-                height={529}
-                layout="responsive"
-                className={styles.image}
-                priority="true"
-              />
+      <figure className={styles.carousel}>
+        <Carousel>
+          <Carousel.Item>
+            <div className={styles.block}>
+              <h1 className={styles.slogan}>
+                <span className={styles.sloganFlash}>Eat</span> your heart out
+              </h1>
+              <div className={styles.imageWrapper}>
+                <Image
+                  alt="Gua Bao"
+                  src={bannerGuabao}
+                  placeholder="blur"
+                  width={800}
+                  height={529}
+                  layout="responsive"
+                  className={styles.image}
+                  priority="true"
+                />
+              </div>
+              <div className={styles.dish}>
+                <DishTile item={eatTile} className={styles.food} />{' '}
+              </div>
             </div>
-            <div className={styles.dish}>
-              <DishTile item={eatTile} className={styles.food} />{' '}
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className={styles.block}>
+              <h1 className={styles.slogan}>
+                <span className={styles.sloganFlash}>Drink</span> and be merry.
+              </h1>
+              <div className={styles.imageWrapper}>
+                <Image
+                  alt="Bubble Milk Tea"
+                  src={bannerMilkTea}
+                  placeholder="blur"
+                  layout="responsive"
+                  width={800}
+                  height={529}
+                  priority
+                  className={styles.image}
+                />
+              </div>
+              <div className={styles.dish}>
+                <DishTile item={drinkTile} />{' '}
+              </div>
             </div>
-          </div>
-        </Carousel.Item>
-        <Carousel.Item>
-          <div className={styles.block}>
-            <h1 className={styles.slogan}>
-              <span className={styles.sloganFlash}>Drink</span> and be merry.
-            </h1>
-            <div className={styles.imageWrapper}>
-              <Image
-                alt="Bubble Milk Tea"
-                src={bannerMilkTea}
-                placeholder="blur"
-                layout="responsive"
-                width={800}
-                height={529}
-                priority
-                className={styles.image}
-              />
-            </div>
-            <div className={styles.dish}>
-              <DishTile item={drinkTile} />{' '}
-            </div>
-          </div>
-        </Carousel.Item>
-      </Carousel>
-      <h1>Wanna try something new?</h1>
-      <p>
-        We've all been there. You walk past a shop with a line out the door, and you take a peek at the menu on the
-        wall.
-      </p>
-      <h3>Bah. All text.</h3>
-      <p>
-        None of the dishes you've learned, and no pictures or translations in sight. Do you brave it, pointing and
-        gesturing towards other people's meals? Or do you eat at the convenience store again in defeat?
-      </p>
-      <h3>I wish I could read Chinese...</h3>
-      <p>
-        You should learn! But its a long road from here to fluency, and your stomach hasn't got that kind of time. Let
-        those further on that road give you a hand along the way with Eatt.
-      </p>
-      <h1>Already comfortable with Chinese?</h1>
-      <p>
-        Have a look around, find something new, and pass it forward! Contribute a missing dish or new menu, and
-        recommend Eatt to a hungry-looking friend.
-      </p>
+          </Carousel.Item>
+        </Carousel>
+      </figure>
+      <section className={styles.text}>
+        <article className={styles.intro}>
+          <h1>Wanna try something new?</h1>
+          <p>
+            We've all been there. You walk past a shop with a line out the door, and you take a peek at the menu on the
+            wall.
+          </p>
+          <h2 className={styles.thought}>Bah. All text.</h2>
+          <p>
+            None of the dishes you've learned, and no pictures or translations in sight. Do you brave it, pointing and
+            gesturing towards other people's meals? Or do you eat at the convenience store again in defeat?
+          </p>
+          <h2 className={styles.thought}>I wish I could read Chinese...</h2>
+          <p>
+            You should learn! But its a long road from here to fluency, and your stomach hasn't got that kind of time.
+            Let those further on that road give you a hand along the way with Eatt.
+          </p>
+        </article>
+        <hr />
+        <article className={styles.action}>
+          <h1>Already comfortable with Chinese?</h1>
+          <p>
+            Have a look around, find something new, and pass it forward! Contribute a missing dish or new menu, and
+            recommend Eatt to a hungry-looking friend.
+          </p>
+        </article>
+      </section>
     </div>
   );
 }
